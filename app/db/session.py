3bb233a9678588ef0fcf203beee46ca90e53f2a5
Nistha <nisthabhattacharjee7@gmail.com>
2026-07-19 16:31:20 +0530

    add sessions

--- app/db/session.py
+++ app/db/session.py
@@ -4,3 +4,19 @@ from  sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()  # Load environment variables from .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    """FastAPI dependency that provides a database session to route functions.
+    Ensures the session is closed after the request finishes, 
+    even if an error occurs during the request processing.
+    """
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()    
+       
