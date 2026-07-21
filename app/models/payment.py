ac92a4d6c396f674c982c54149241ebe93a822c1
Nistha <nisthabhattacharjee7@gmail.com>
2026-07-22 03:08:05 +0530

    Add Payment model with merchant FK, status enum, and integer amount

--- app/models/payment.py
+++ app/models/payment.py
@@ -1,5 +1,27 @@
 import uuid 
 from datetime import datetime, timezone
-from sqlalchemy import String, DateTime, ForeinKey, Integer, Enum as SAEnum
+from sqlalchemy import ForeignKey, String, DateTime, ForeinKey, Integer, Enum as SAEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.base import Base
+from app.config.enums import Currency, PaymentStatus, PaymentMethod
+from app.models.merchant import Merchant
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    merchant_id: Mapped[str] = mapped_column(String, ForeignKey("merchants.id"), nullable=False)
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    currency: Mapped[Currency] = mapped_column(SAEnum(Currency), nullable=False , default = Currency.INR) 
+    status: Mapped[PaymentStatus] = mapped_column(SAEnum(PaymentStatus), default=PaymentStatus.CREATED, nullable=False)
+    receipt: Mapped[str] = mapped_column(String, nullable=True)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),
+                                                default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), 
+                                                default=lambda: datetime.now(timezone.utc),
+                                                            onupdate=lambda: datetime.now(timezone.utc))
+
+    merchant: Mapped["Merchant"] = relationship("Merchant", back_populates="payments")
+
