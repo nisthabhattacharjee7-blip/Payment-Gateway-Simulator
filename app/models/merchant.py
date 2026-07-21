88ae22a771fd458e2c20be5a87fa144da15c699b
Nistha <nisthabhattacharjee7@gmail.com>
2026-07-22 01:40:43 +0530

    Add Merchant model with UUID primary key and API key auth field

--- app/models/merchant.py
+++ app/models/merchant.py
@@ -4,3 +4,15 @@ from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base import Base
 
+class Merchant(Base):
+    __tablename__ = "merchants"
+
+    id: Mapped[str] = mapped_column(String , primary_key=True, default=lambda: str(uuid.uuid4()))
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    api_key: Mapped[str] = mapped_column(String, unique=True, default=lambda: str(uuid.uuid4()), nullable=False)
+    webhook_url: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+
